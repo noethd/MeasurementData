d4b3bb641bbed4a2aabd955bb6595eece2d48287
Dominik <dominik@macbook.fritz.box>
2023-05-31 08:59:39 +0200

    added printout

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ void runExamples();
 int main() {
     /* we will use a simple 4*4 matrix, predefined here: */
     const unsigned char buffer[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 1, 2, 3, 4};
+    MD_printMatrix(buffer,4,4);
 
     /* create measurement data matrix */
     MD *pBuffer = MD_create(buffer, 4, 4);
